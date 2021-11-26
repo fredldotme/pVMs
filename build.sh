c60ee8d21ef8bc7c534e528c760bb78db9ffe879
Alfred Neumayer <dev.beidl@gmail.com>
2021-11-26 19:04:58 +0100

    build: Avoid stripping QEMU binaries
    Fails for some reason, let's keep symbols for now.

--- build.sh
+++ build.sh
@@ -107,7 +107,7 @@ function build_3rdparty_cmake {
 
 ninja --version
 
-build_3rdparty_autogen qemu "--python=/usr/bin/python3.6 --enable-sdl --audio-drv-list=pa,sdl --target-list=aarch64-softmmu,x86_64-softmmu"
+build_3rdparty_autogen qemu "--python=/usr/bin/python3.6 --enable-sdl --audio-drv-list=pa,sdl --target-list=aarch64-softmmu,x86_64-softmmu --disable-strip"
 
 # Build main sources
 build_project
