c155e63d763ad9c3ae0ad66551046f899e8f744d
mateosalta <mateo_salta@yahoo.com>
2021-12-09 03:02:26 -0600

    qrc path change

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     qDebug() << "Starting app from main.cpp";
 
     QQuickView *view = new QQuickView();
-    view->setSource(QUrl("qrc:/Main.qml"));
+    view->setSource(QUrl("qrc:///Main.qml"));
     view->setResizeMode(QQuickView::SizeRootObjectToView);
     view->show();
 
