6af7bcd24245c86fedd806284e3a97f9ee067373
Alfred E. Neumayer <dev.beidl@gmail.com>
2024-09-18 09:42:19 +0200

    plugins: Include missing QGuiApplication header

--- src/plugins/PocketVMs/machine.cpp
+++ src/plugins/PocketVMs/machine.cpp
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <QCoreApplication>
+#include <QGuiApplication>
 #include <QDebug>
 #include <QDir>
 #include <QFile>
